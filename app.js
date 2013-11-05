9447bdc4bbd6dac250f85610945a45da9d21a745
Shayne Kasai <shayne@ALCAZAR.local>
2013-11-05 07:37:10 -0800

    Renamed test -> Test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var server = require("./lib/server");
 var router = require("./lib/router");
-var test   = require("./lib/test");
+var test   = require("./lib/Test");
 
-var objTest = new test();
+var objTest = new Test();
 
 var handle = {}
 handle['/db-read'] = objTest.exampleQuery;
