1b9d15c1b3a0576fccdf602b19fe3097f06801c2
Shayne Kasai <shayne@ALCAZAR.local>
2013-11-29 18:19:49 -0800

    Dummy code

--- api.js
+++ api.js
@@ -1,1 +1,16 @@
-/* this is meant to be the REST api server */
+var restify = require('restify');
+
+var server = restify.createServer({
+  name: 'API'
+});
+
+
+ function send(req, res, next) {
+   res.send('hello ' + req.params.name);
+   return next();
+ }
+
+ server.get('/hello/:name', send);
+
+
+server.listen(8080);
