b8ccfa150a8fa066a15dccf5e344e8d440a1a93e
Shayne Kasai <shayne@ALCAZAR.local>
2013-11-30 09:32:19 -0800

    Fixing up

--- api.js
+++ api.js
@@ -1,14 +1,20 @@
 var restify = require('restify');
+var Login   = require('./lib/Login');
 
 var server = restify.createServer({
     name: 'API'
 });
+var objLogin = new Login();
+
 
 
 function send(req, res, next) {
     //res.send('hello ' + req.params.name);
+    res.send('hello world');
     return next();
 }
+
+server.get('/api/v1/get-token-test', objLogin.authTest); // dummy test
 
 server.get('/api/v1/get-user:username', send);
 server.post('/api/v1/update-user:username', send);
