d3bece30c2fcb58cf595997ddc470d566f579360
Shayne Kasai <shayne@ALCAZAR.local>
2013-11-30 08:50:23 -0800

    testing rest

--- api.js
+++ api.js
@@ -1,16 +1,17 @@
 var restify = require('restify');
 
 var server = restify.createServer({
-  name: 'API'
+    name: 'API'
 });
 
 
- function send(req, res, next) {
-   res.send('hello ' + req.params.name);
-   return next();
- }
+function send(req, res, next) {
+    //res.send('hello ' + req.params.name);
+    return next();
+}
 
- server.get('/hello/:name', send);
-
+server.get('/api/v1/get-user:username', send);
+server.post('/api/v1/update-user:username', send);
+server.post('/api/v1/create-user:username', send);
 
 server.listen(8080);
